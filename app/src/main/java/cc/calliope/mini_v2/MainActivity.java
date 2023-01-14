5452e2d7ba8e07084d9b6415e030f471ae2643e7
Tsisar <fhantom.tp@gmail.com>
2023-01-14 19:21:39 +0100

    add fullscreen mode

--- app/src/main/java/cc/calliope/mini_v2/MainActivity.java
+++ app/src/main/java/cc/calliope/mini_v2/MainActivity.java
@@ -1,31 +1,19 @@
 package cc.calliope.mini_v2;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.res.ColorStateList;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -33,12 +21,9 @@ import java.util.List;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -61,7 +46,6 @@ import cc.calliope.mini_v2.views.MovableFloatingActionButton;
 public class MainActivity extends AppCompatActivity implements DialogInterface.OnDismissListener {
     private static final int REQUEST_CODE = 1022; // random number
     private static final int SNACKBAR_DURATION = 10000; // how long to display the snackbar message.
-    private static final String CHANNEL_ID = "145";
     private static boolean requestWasSent = false;
 
     private ActivityMainBinding binding;
@@ -74,9 +58,9 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
     private Button actionButton;
     private Button settingsButton;
 
-    private Boolean gone = false;
+    private ImageButton fullScreenButton;
 
-    BroadcastReceiver broadcastReceiver;
+    private Boolean isFullScreen = false;
 
     ActivityResultLauncher<Intent> bluetoothEnableResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -107,72 +91,33 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         actionButton.setOnClickListener(v -> requestPermissions());
         settingsButton.setOnClickListener(v -> requestAppSettings());
 
-        broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
+        if(fullScreenButton != null){
+            fullScreenButton.setOnClickListener(this::setFullScreenMode);
+        }
 
-                NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-                manager.cancelAll();
-
-                String state = intent.getAction();
-                if (state.equals("Yes")) {
-                    fullScreen();
-                }
-            }
-        };
-
-        fsNotify();
+//        broadcastReceiver = new BroadcastReceiver() {
+//            @Override
+//            public void onReceive(Context context, Intent intent) {
+//
+//                NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+//                manager.cancelAll();
+//
+//                String state = intent.getAction();
+//                if (state.equals("Yes")) {
+//                    fullScreen();
+//                }
+//            }
+//        };
     }
 
     @Override
     public void onBackPressed() {
-        if(gone){
-            fullScreen();
-        }else {
+        if (isFullScreen) {
+            isFullScreen = false;
+            binding.bottomNavigation.setVisibility(View.VISIBLE);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
             super.onBackPressed();
-        }
-    }
-
-    private void fsNotify() {
-        createNotificationChannel();
-
-        Intent intent = new Intent(); //same
-        intent.setAction("Yes");
-        intent.putExtra("RES", true);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-        PendingIntent pendingIntent1 = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_IMMUTABLE);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_connect)
-                .setContentTitle("My notification")
-                .setContentText("Much longer text that cannot fit one line...")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .addAction(R.drawable.ic_device_black_24dp, "Yes", pendingIntent1);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(125, builder.build());
-
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction("Yes");
-
-        registerReceiver(broadcastReceiver, intentFilter);
-    }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "channel_name";
-            String description = "channel_description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
         }
     }
 
@@ -183,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         settingsButton = binding.noPermissionLayout.settingsButton;
         mainLayout = binding.mainLayout;
         noPermissionLayout = binding.noPermissionLayout.getRoot();
+        fullScreenButton = binding.fullscreenButton;
     }
 
     @Override
@@ -228,15 +174,10 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         ));
     }
 
-    private void fullScreen() {
-        if (gone) {
-            binding.bottomNavigation.setVisibility(View.VISIBLE);
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        } else {
-            binding.bottomNavigation.setVisibility(View.GONE);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
-        gone = !gone;
+    private void setFullScreenMode(View view) {
+        isFullScreen = true;
+        binding.bottomNavigation.setVisibility(View.GONE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     private int getColorWrapper(int id) {
